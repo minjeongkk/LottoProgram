37e2f692b9f050a1d881563c5f9b79dccc843fa4
minjeongkk <kminjeong01@naver.com>
2023-09-12 09:02:17 +0900

    feat : 구분자를 기준으로 문자열을 자르는 기능 구현

--- src/main/java/StringAddCalculator.java
+++ src/main/java/StringAddCalculator.java
@@ -8,6 +8,7 @@ public class StringAddCalculator {
 
     public static int splitAndSum(String inputString){
         String string = "";     // 문자열
+        String[] strings = new String[]{};  // 구분자로 나눈 문자열
 
         // 유효한 문자열인지 확인
         if(!isValidString(inputString)){
@@ -17,6 +18,11 @@ public class StringAddCalculator {
         // 커스텀 구분자 지정
         // 입력 문자열 재지정
         string = getString(inputString);
+
+        // 문자열 자르기
+        strings = splitString(string);
+
+
 
         return 1;
     }
@@ -63,4 +69,14 @@ public class StringAddCalculator {
         String addRegex = "|" + customRegex;
         regex += addRegex;
     }
+
+    /*
+    * 구분자를 기준으로 문자열을 자르는 함수
+    * */
+    public static String[] splitString(String inputString){
+        String[] strings = inputString.split(regex);
+        return strings;
+    }
+
+
 }

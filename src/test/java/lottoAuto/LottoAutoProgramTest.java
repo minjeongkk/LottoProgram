c556756dfcfeb3976fc74a94f1735ef89884f193
minjeongkk <kminjeong01@naver.com>
2023-09-15 10:08:40 +0900

    test : 기능별 테스트 코드 작성

--- src/test/java/lottoAuto/LottoAutoProgramTest.java
+++ src/test/java/lottoAuto/LottoAutoProgramTest.java
@@ -2,6 +2,10 @@ package lottoAuto;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -26,5 +30,28 @@ class LottoAutoProgramTest {
 
         int actual = lottoAutoProgram.countLotto(12000);
         assertThat(actual).isEqualTo(12);
+    }
+
+    @Test
+    public void createLotto(){
+        LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
+        List<Integer> list = lottoAutoProgram.createLotto();
+        assertThat(list).allMatch(nums -> nums.size()==6);
+    }
+
+    @Test
+    public void calculateRate(){
+        LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
+        float expected = 0.35f;
+        float actual = lottoAutoProgram.calculateRate(14000, new int[]{1,0,0,0});
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    public void printResult(){
+        LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
+        String expected = "손해";
+        String actual = lottoAutoProgram.checkResult(0.35f);
+        assertThat(actual).isEqualTo(expected);
     }
 }

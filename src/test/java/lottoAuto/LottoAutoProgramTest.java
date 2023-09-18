086dd0c6fbe1038eda1e68bb84fbada0ad244e8b
minjeongkk <kminjeong01@naver.com>
2023-09-18 15:30:42 +0900

    test : 기능별 테스트 코드 수정/추가

--- src/test/java/lottoAuto/LottoAutoProgramTest.java
+++ src/test/java/lottoAuto/LottoAutoProgramTest.java
@@ -25,7 +25,7 @@ class LottoAutoProgramTest {
     }
 
     @Test
-    public void countLotto(){
+    public void countLotto() {
         LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
 
         int actual = lottoAutoProgram.countLotto(12000);
@@ -33,25 +33,67 @@ class LottoAutoProgramTest {
     }
 
     @Test
-    public void createLotto(){
+    public void createLotto() {
         LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
         List<Integer> list = lottoAutoProgram.createLotto();
-        assertThat(list).allMatch(nums -> nums.size()==6);
+        assertThat(list.size()).isEqualTo(6);
     }
 
     @Test
-    public void calculateRate(){
+    public void splitNums_숫자가_아닌_경우(){
         LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
-        float expected = 0.35f;
-        float actual = lottoAutoProgram.calculateRate(14000, new int[]{1,0,0,0});
+        assertThatThrownBy(() -> lottoAutoProgram.splitNums("abcd"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitNums_범위_벗어난_경우(){
+        LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
+        assertThatThrownBy(() -> lottoAutoProgram.splitNums("46, 47, 48, 49, 50, -1"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void splitNums_개수6개_아닌_경우(){
+        LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
+        assertThatThrownBy(() -> lottoAutoProgram.splitNums("1,2"))
+                .isInstanceOf(RuntimeException.class);
+        assertThatThrownBy(() -> lottoAutoProgram.splitNums("1, 2, 1, 2, 1, 2"))
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void countMatchValue() {
+        LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
+        int[] expected = new int[]{1, 1, 0, 0};
+
+        int[] lottoNums = new int[]{1, 2, 3, 4, 5, 6};
+        List<List<Integer>> lottoList = new ArrayList<>();
+        lottoList.add(Arrays.asList(1, 2, 3, 7, 8, 9));
+        lottoList.add(Arrays.asList(1, 2, 3, 4, 43, 41));
+        lottoList.add(Arrays.asList(12, 22, 32, 44, 42, 10));
+        int[] result = lottoAutoProgram.countMatchValue(lottoNums, lottoList);
+
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    public void calculateRate() {
+        LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
+        float expected = 0.36f;
+        float actual = lottoAutoProgram.calculateRate(14000, new int[]{1, 0, 0, 0});
         assertThat(actual).isEqualTo(expected);
     }
 
     @Test
-    public void printResult(){
+    public void printResult() {
         LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
         String expected = "손해";
         String actual = lottoAutoProgram.checkResult(0.35f);
         assertThat(actual).isEqualTo(expected);
+
+        String expected2 = "이득";
+        String actual2 = lottoAutoProgram.checkResult(1.35f);
+        assertThat(actual2).isEqualTo(expected2);
     }
 }

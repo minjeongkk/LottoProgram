493f8637822b6eebc7c14127c21c90355f1e04f3
minjeongkk <kminjeong01@naver.com>
2023-09-18 15:38:09 +0900

    feat : 구입 개수만큼 로또를 생성하는 함수 구현

--- src/main/java/lottoAuto/LottoAutoProgram.java
+++ src/main/java/lottoAuto/LottoAutoProgram.java
@@ -37,6 +37,39 @@ public class LottoAutoProgram {
         return count;
     }
 
+    /*
+     * 구매 개수만큼 로또를 사는 함수
+     *
+     * 구매한 로또의 값들을 리스트로 반환
+     * */
+    public List<List<Integer>> buyLotto(int size) {
+        List<List<Integer>> purchaseLotto = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            List<Integer> lotto = createLotto();    // 로또 1개 생성
+            purchaseLotto.add(lotto);
+        }
+        return purchaseLotto;
+    }
+
+    /*
+     * 로또 1개를 생성하는 함수
+     *
+     * 1~45 사이의 숫자가 있는 리스트를 섞어 6번째 숫자까지 반환
+     * */
+    public List<Integer> createLotto() {
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 1; i <= 45; i++) {
+            nums.add(i);
+        }
+        Collections.shuffle(nums);
+
+        List<Integer> lottoNums = nums.subList(0, 6);
+        Collections.sort(lottoNums);
+
+        return lottoNums;
+    }
+
+
 
     /*
     * 로또(자동) 프로그램 동작 함수
@@ -50,6 +83,11 @@ public class LottoAutoProgram {
 
         // 구입로또 개수 셈
         int size = countLotto(money);
+
+        // 구입 개수만큼 로또 생성 및 출력
+        List<List<Integer>> lottoList = buyLotto(size);
+        lottoAutoProgramUI.printPurchaseList(size, lottoList);
+
 
     }
 }

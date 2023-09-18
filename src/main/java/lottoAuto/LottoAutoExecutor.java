af4d6257e6ef5879d149dd8e8ea033d3e985501d
minjeongkk <kminjeong01@naver.com>
2023-09-18 15:31:38 +0900

    feat : 로또(자동) 프로그램 시작 코드 구현

---
+++ src/main/java/lottoAuto/LottoAutoExecutor.java
@@ -0,0 +1,10 @@
+package lottoAuto;
+
+public class LottoAutoExecutor {
+
+    public static void main(String[] args) {
+        // 로또(자동) 프로그램 시작
+        LottoAutoProgram lottoAutoProgram = new LottoAutoProgram();
+        lottoAutoProgram.run();
+    }
+}

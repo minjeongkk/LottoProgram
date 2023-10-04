af41261bb4c0fe83c88a5a9807f0709018b0cf1d
minjeongkk <kminjeong01@naver.com>
2023-10-04 16:06:33 +0900

    feat : 당첨 통계 출력 화면 구현

--- src/main/java/lottoSecond/LottoSecondProgramUI.java
+++ src/main/java/lottoSecond/LottoSecondProgramUI.java
@@ -23,4 +23,15 @@ public class LottoSecondProgramUI {
     public void printInputBonus() {
         System.out.println("보너스 볼을 입력해 주세요.");
     }
+
+    public void printResult(Map<Rank, Integer> matchNums, float rateOfReturn, String result) {
+        System.out.println("당첨 통계");
+        System.out.println("---------");
+        System.out.printf("3개 일치 (5000원)- %d개\n", matchNums.get(Rank.FIFTH));
+        System.out.printf("4개 일치 (50000원)- %d개\n", matchNums.get(Rank.FOURTH));
+        System.out.printf("5개 일치 (1500000원)- %d개\n", matchNums.get(Rank.THIRD));
+        System.out.printf("5개 일치, 보너스 볼 일치(30000000원) - %d개\n", matchNums.get(Rank.SECOND));
+        System.out.printf("6개 일치 (2000000000원)- %d개\n", matchNums.get(Rank.FIRST));
+        System.out.printf("총 수익률은 %s입니다.(기준이 1이기 때문에 결과적으로 %s라는 의미임)", rateOfReturn, result);
+    }
 }

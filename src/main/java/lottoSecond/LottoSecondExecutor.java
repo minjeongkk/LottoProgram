9cae3f434b70e518ec696d6754abc16029911fb0
minjeongkk <kminjeong01@naver.com>
2023-10-04 16:03:36 +0900

    feat : 로또(2등) 프로그램 실행기 구현

---
+++ src/main/java/lottoSecond/LottoSecondExecutor.java
@@ -0,0 +1,10 @@
+package lottoSecond;
+
+public class LottoSecondExecutor {
+
+    public static void main(String[] args) {
+        // 로또(2등) 프로그램 시작
+        LottoSecondProgram lottoSecondProgram = new LottoSecondProgram();
+        lottoSecondProgram.run();
+    }
+}

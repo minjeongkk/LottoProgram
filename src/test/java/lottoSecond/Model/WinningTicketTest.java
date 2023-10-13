654b5243484eba4ddf90f6ccf510dbf438f14f37
minjeongkk <kminjeong01@naver.com>
2023-10-13 14:25:43 +0900

    refactor : Model WinningTicketTest test 코드 수정

--- src/test/java/lottoSecond/Model/WinningTicketTest.java
+++ src/test/java/lottoSecond/Model/WinningTicketTest.java
@@ -10,9 +10,27 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class WinningTicketTest {
     @Test
-    public void setBonusBall_실패(){
+    public void setBonusBall_실패_문자(){
         List<Integer> lottonums = Arrays.asList(1, 2, 3, 7, 8, 9);
-        int bonusBall = 1;
+        String bonusBall = "ㄱ";
+        WinningTicket winningTicket = new WinningTicket(lottonums);
+        assertThatThrownBy(() -> {winningTicket.checkBonusNum(bonusBall);})
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void setBonusBall_실패_범위(){
+        List<Integer> lottonums = Arrays.asList(1, 2, 3, 7, 8, 9);
+        String bonusBall = "100";
+        WinningTicket winningTicket = new WinningTicket(lottonums);
+        assertThatThrownBy(() -> {winningTicket.checkBonusNum(bonusBall);})
+                .isInstanceOf(RuntimeException.class);
+    }
+
+    @Test
+    public void setBonusBall_실패_중복(){
+        List<Integer> lottonums = Arrays.asList(1, 2, 3, 7, 8, 9);
+        String bonusBall = "1";
         WinningTicket winningTicket = new WinningTicket(lottonums);
         assertThatThrownBy(() -> {winningTicket.checkBonusNum(bonusBall);})
                 .isInstanceOf(RuntimeException.class);
@@ -21,37 +39,9 @@ class WinningTicketTest {
     @Test
     public void setBonusBall_성공(){
         List<Integer> lottonums = Arrays.asList(1, 2, 3, 7, 8, 9);
-        int bonusBall = 10;
+        String bonusBall = "10";
         WinningTicket winningTicket = new WinningTicket(lottonums);
         winningTicket.checkBonusNum(bonusBall);
         assertThat(winningTicket.getBonusBall()).isEqualTo(10);
-    }
-
-    @Test
-    public void setNums_실패_중복(){
-        List<Integer> lottonums = Arrays.asList(1, 2, 3, 7, 8, 8);
-        WinningTicket winningTicket = new WinningTicket(lottonums);
-        assertThat(winningTicket.getLottoNums()).isEqualTo(Arrays.asList(1, 2, 3, 7, 8, 9));
-    }
-
-    @Test
-    public void setNums_실패_범위벗어남(){
-        List<Integer> lottonums = Arrays.asList(1, 2, 3, 7, 8, 50);
-        WinningTicket winningTicket = new WinningTicket(lottonums);
-        assertThat(winningTicket.getLottoNums()).isEqualTo(Arrays.asList(1, 2, 3, 7, 8, 9));
-    }
-
-    @Test
-    public void setNums_실패_개수(){
-        List<Integer> lottonums = Arrays.asList(1, 2, 3);
-        WinningTicket winningTicket = new WinningTicket(lottonums);
-        assertThat(winningTicket.getLottoNums()).isEqualTo(Arrays.asList(1, 2, 3, 7, 8, 9));
-    }
-
-    @Test
-    public void setNums_성공(){
-        List<Integer> lottonums = Arrays.asList(1, 2, 3, 7, 8, 9);
-        WinningTicket winningTicket = new WinningTicket(lottonums);
-        assertThat(winningTicket.getLottoNums()).isEqualTo(Arrays.asList(1, 2, 3, 7, 8, 9));
     }
 }
